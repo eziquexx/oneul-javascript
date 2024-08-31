2d0eb130936db3942ea1e46f4518902f32a11cb3
eziquexx <waftyann@gmail.com>
2024-08-31 23:31:36 +0900

    플레이어 움직이게 만들기

--- oneul-js-dong-practice/js/start-2.js
+++ oneul-js-dong-practice/js/start-2.js
@@ -3,13 +3,24 @@ const startBtn = document.getElementById("startBtn");
 const titleGroup = document.querySelector(".titleGroup");
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+const leftDogImg = './img/dogLeft.png';
+const rightDogImg = './img/dogRight.png';
+
+
+// Poo 객체
+const Poo = {
+  
+}
 
 
 // Dog 객체
 const Dog = {
   width: 100,
   height: 100,
+  x: 0,
+  y: 0,
   image: new Image(),
+  step: 20,
 
   // 이미지 로드와 그리기 함수
   init: function(src) {
@@ -17,6 +28,9 @@ const Dog = {
     this.image.onload = () => {
       // 이미지 로드 후 캔버스 초기화
       resizeCanvas();
+      this.x = (canvas.width - this.width) / 2; // 가운데 오게
+      this.y = canvas.height - this.height - 20; // 아래 기준 20
+      this.draw();
     }
   },
 
@@ -24,23 +38,33 @@ const Dog = {
   draw: function() {
     // 이미지가 로드되지 않았으면 그리지 않기.
     if (!this.image.complete) return;
+    if (!Game.started) return; // 게임 시작 않았으면 이동x
 
-    const { width: canvasWidth, height: canvasHeight } = canvas;
-    const x = (canvasWidth - this.width) / 2; // 가운데 오게
-    const y = canvasHeight - this.height - 20; // 아래 기준 20
-    
     // clearRect로 캔버스 초기화시키기
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    ctx.drawImage(this.image, x, y, this.width, this.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
   },
 
   // dog 이미지 좌,우 방향키에 맞추어 움직이기
-  move: function(event) {
+  move: function(e) {
+    if (!e) return; // 이벤트가 없을 경우 처리
+
     const arrowL = "ArrowLeft";
     const arrowR = "ArrowRight";
-    if(event === arrowL) {}
-    if(event === arrowR) {}
+    if (arrowL === e.key) {
+      this.x = Math.max(this.x - this.step, 0);
+      this.image.src = leftDogImg;
+      this.image.onload = () =>  this.draw();
+    }
+    if (arrowR === e.key) {
+      this.x = Math.min(this.x + this.step, canvas.width - this.width);
+      this.image.src = rightDogImg;
+      this.image.onload = () =>  this.draw();
+    }
+
+    this.draw(); // 이동 후 다시 그리기
   }
+
 };
 
 
@@ -62,16 +86,6 @@ const Game = {
     }
   },
 
-  arrowKey: function(event) {
-    const arrowL = "ArrowLeft";
-    const arrowR = "ArrowRight";
-    if (arrowL === event.key) {
-      console.log("왼쪽");
-    }
-    if (arrowR === event.key) {
-      console.log("오른쪽");
-    }
-  }
 };
 
 
@@ -89,7 +103,7 @@ startBtn.addEventListener("click", () => Game.start() );
 window.addEventListener("resize", () => Game.resize() );
 
 // 이미지 초기화
-Dog.init('./img/dogRight2.png');
+Dog.init(rightDogImg);
 
-
-window.addEventListener("keydown", () => Game.arrowKey(event) );
+// key값 받아서 Dog.move로 전달
+window.addEventListener("keydown", (event) => Dog.move(event) );
--- oneul-js-dong/js/dog.js
+++ oneul-js-dong/js/dog.js
@@ -9,18 +9,18 @@ var Dog = {
         this.dog.src = 'image/dogRight.png';     
         context.drawImage(this.dog,this.x,this.y,this.dogW,this.dogH);          
     },
-    // move: function(){      
-    //     if( direction == 'right'){
-    //         this.x = this.x+this.moveSpace;
-    //         if( this.x > canvasW-this.dogW)
-    //             this.x = canvasW-this.dogW;        
-    //     }    
-    //     if( direction == 'left'){        
-    //         this.x = this.x-this.moveSpace; 
-    //         if( this.x < 0)
-    //             this.x = 0;                   
-    //     }    
-    //     direction = '';   
-    // }
+    move: function(){      
+        if( direction == 'right'){
+            this.x = this.x+this.moveSpace;
+            if( this.x > canvasW-this.dogW)
+                this.x = canvasW-this.dogW;        
+        }    
+        if( direction == 'left'){        
+            this.x = this.x-this.moveSpace; 
+            if( this.x < 0)
+                this.x = 0;                   
+        }    
+        direction = '';   
+    }
 }
 // 블록 객체에 사각형을 만드는 메소드 생성
